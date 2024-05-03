0478beba90707ac1d39aad3496ed12650e1acfb0
Mondo <mondo.jiang@wisc.edu>
2024-05-02 22:21:19 -0500

    build: fix next build

--- next.config.js
+++ next.config.js
@@ -2,14 +2,15 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
-  output: "export",
+  output: 'export',
   images: {
     unoptimized: true,
   },
 };
 
-const withNextra = require('nextra')(nextConfig);
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+});
 
-module.exports = withNextra();
+module.exports = { ...nextConfig, ...withNextra() };
