3f47d4121f78532e1d31b4b83668857c3296c2c7
Mondo <mondo.jiang@wisc.edu>
2024-05-03 18:52:50 -0500

    feat: rewrite dataset page

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,29 +1,31 @@
 export default {
-    logo: <span>Real-time Crowd Detection for Shared Facilities</span>,
-    docsRepositoryBase: "https://github.com/uwm-cs766-24spring-team/psychic-umbrella",
-    project: {
-      link: 'https://github.com/uwm-cs766-24spring-team/psychic-umbrella'
-    },
-    chat: { },
-    sidebar: {
-      toggleButton: true,
-    },
-    footer:{
-      text:<span>2024 © Made with ❤️ by Lao, Mondo, and William</span>,
-    },
-    useNextSeoProps() {
-      return {
-        titleTemplate: '%s – Crowd Detection for Shared Facilities - CS766 Project',
-      }
-    },
-    toc: {
-      backToTop: true
-    },
-    editLink: {
-      component: null
-    },
-    feedback: {
-      content: 'Question? Give us feedback →',
-      labels: 'feedback'
-    }
-  }
+  logo: <span>Real-time Crowd Detection for Shared Facilities</span>,
+  docsRepositoryBase:
+    'https://github.com/uwm-cs766-24spring-team/psychic-umbrella',
+  project: {
+    link: 'https://github.com/uwm-cs766-24spring-team/psychic-umbrella',
+  },
+  chat: {},
+  sidebar: {
+    toggleButton: true,
+  },
+  footer: {
+    text: <span>2024 © Made with ❤️ by Lao, Mondo, and William</span>,
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate:
+        '%s – Crowd Detection for Shared Facilities - CS766 Project',
+    };
+  },
+  toc: {
+    backToTop: true,
+  },
+  editLink: {
+    component: null,
+  },
+  feedback: {
+    content: 'Question? Give us feedback →',
+    labels: 'feedback',
+  },
+};
